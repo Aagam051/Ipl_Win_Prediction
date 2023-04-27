a459af5af51e2a701e7a0b6caa42c1ec7c3950ae
Aagam Shah <71314883+Aagam051@users.noreply.github.com>
2023-04-27 08:59:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,16 +42,25 @@ with col5:
     wickets = st.number_input('Wickets out')
 
 if st.button('Predict Probability'):
-    runs_left = target - score
-    balls_left = 120 - (overs*6)
-    wickets = 10 - wickets
-    crr = score/overs
-    rrr = (runs_left*6)/balls_left
+   if score>=target:
+        st.error("Score should be less than Target")
+    elif overs>=20 and overs>0:
+        st.error("Overs should be Greater than 0 and Less than or equal to 19")
+    elif wickets>=10:
+        st.error("Wickets should be less than 10")
+    elif batting_team == bowling_team:
+        st.error("Bowling and batting team can't be same!")
+    else:
+        runs_left = target - score
+        balls_left = 120 - (overs*6)
+        wickets = 10 - wickets
+        crr = score/overs
+        rrr = (runs_left*6)/balls_left
 
-    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
+        input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
 
-    result = pipe.predict_proba(input_df)
-    loss = result[0][0]
-    win = result[0][1]
-    st.header(batting_team + "- " + str(round(win*100)) + "%")
-    st.header(bowling_team + "- " + str(round(loss*100)) + "%")
+        result = pipe.predict_proba(input_df)
+        loss = result[0][0]
+        win = result[0][1]
+        st.header(batting_team + "- " + str(round(win*100)) + "%")
+        st.header(bowling_team + "- " + str(round(loss*100)) + "%")
